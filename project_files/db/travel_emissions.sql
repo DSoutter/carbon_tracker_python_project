b58a34b3545cd388a319d7a8e9973421bad3051f
DSoutter <duncan.soutter@gmail.com>
2021-08-13 15:12:35 +0100

    tables created

--- project_files/db/travel_emissions.sql
+++ project_files/db/travel_emissions.sql
@@ -1,5 +1,24 @@
--- Ordering matters, ones with references to trips before trips.
+-- Ordering matters, ones with references to others first (trips).
+DROP TABLE IF EXISTS trips;
 DROP TABLE IF EXISTS transport_types;
 DROP TABLE IF EXISTS purposes;
-DROP TABLE IF EXISTS trips;
 
+CREATE TABLE purposes (
+    id SERIAL PRIMARY KEY,
+    travel_purpose VARCHAR(255)
+);
+
+CREATE TABLE transport_types (
+    id SERIAL PRIMARY KEY,
+    mode_of_travel INT,
+    carbon_per_mile INT
+);
+
+CREATE TABLE trips (
+    id SERIAL PRIMARY KEY,
+    distance INT,
+    carbon INT,
+    date DATE, 
+    purpose_id INT REFERENCES purposes(id),
+    transport_type_id INT REFERENCES transport_types(id)
+);
