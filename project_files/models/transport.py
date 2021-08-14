144021231d9e97a4f5a84248bc4e8043b20b16e1
DSoutter <duncan.soutter@gmail.com>
2021-08-14 17:48:35 +0100

    pseudocode for emissions written

--- project_files/models/transport.py
+++ project_files/models/transport.py
@@ -5,4 +5,18 @@ class Transport:
         self.emissions_per_mile = emissions_per_mile
         self.id = id
 
+    def emissions_pm(self):
+        # return self.mpg * some factor
+        # could consider it an if else statement as follows:
+        # if mode is car:
+        #   if car is diesel:
+        #       return factor x mpg
+        #  else:
+        #       return factor x mpg
+        # else if mode is plane:
+        #   return a predetermined number
+        # else if mode is bus:
+        #   return a predetermined number
+        # else if mode is train:
+        #   return a predetermined number
         
