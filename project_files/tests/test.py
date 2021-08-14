6df794e1c49e10dc81dbb09324e3ba18c9244b2d
DSoutter <duncan.soutter@gmail.com>
2021-08-14 21:18:27 +0100

    added a test setup

--- project_files/tests/test.py
+++ project_files/tests/test.py
@@ -2,7 +2,25 @@ import unittest
 from models.purpose import *
 from models.transport import *
 from models.trip import *
+import datetime
 
 from repositories.purpose_repository import *
 from repositories.transport_repository import *
 from repositories.trip_repository import *
+
+class TestClasses(unittest.TestCase):
+    def setUp(self):
+        self.purpose1 = Purpose("To/From Work")
+        self.purpose2 = Purpose("For Work Travel")
+        self.purpose3 = Purpose("Kids Taxi Service")
+        self.purpose4 = Purpose("Leisure")
+
+        self.transport1 = Transport("Car 1, petrol", 45)
+        self.transport2 = Transport("Car 2, diesel", 50)
+        self.transport3 = Transport("Bus", None)
+        self.transport4 = Transport("Plane", None)
+        self.transport5 = Transport("Train", None)
+
+        self.trip1 = Trip(40, datetime.date(2021, 8, 10), self.purpose1, self.transport1)
+        self.trip2 = Trip(60, datetime.date(2021, 8, 11), self.purpose1, self.transport1)
+        self.trip3 = Trip(500, datetime.date(2021, 8, 12), self.purpose2, self.transport4)
