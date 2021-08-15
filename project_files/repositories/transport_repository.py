3e23a752a7b289313763eea5ea7209969ba29d03
DSoutter <duncan.soutter@gmail.com>
2021-08-15 15:17:52 +0100

    Transport repo set up

---
+++ project_files/repositories/transport_repository.py
@@ -0,0 +1,30 @@
+from pdb import run
+
+from psycopg2.extensions import SQL_IN
+from db.run_sql import run_sql
+
+from models.transport import Transport
+
+# save
+
+
+
+# select all
+
+
+
+# select
+
+
+
+# delete
+
+
+
+# delete all
+
+
+
+# update
+
+
