45e40e424b1e036b695ef8dd618d3a23ac83c63a
DSoutter <duncan.soutter@gmail.com>
2021-08-15 19:39:38 +0100

    added index for travel purposes

--- project_files/controllers/purposes_controller.py
+++ project_files/controllers/purposes_controller.py
@@ -10,11 +10,13 @@ purposes_blueprint = Blueprint("purposes", __name__)
 @purposes_blueprint.route("/purposes")
 def purposes():
     purposes = purpose_repo.select_all()
-    return render_template("humans/index.html", purposes = purposes)
+    return render_template("purposes/index.html", purposes = purposes)
 
 # New (add a new purpose screen)
 
-
+@purposes_blueprint.route("/purposes/new")
+def new_purpose():
+    return render_template("purposes/new.html")
 
 # Create (posting the new purpose to the list)
 
