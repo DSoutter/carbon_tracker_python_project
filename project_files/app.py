d0f4e6cf5491d5e70e9ebde77ea424e74e54c441
DSoutter <duncan.soutter@gmail.com>
2021-08-15 19:00:59 +0100

    added index.html file

--- project_files/app.py
+++ project_files/app.py
@@ -1,1 +1,17 @@
 from flask import Flask, render_template
+
+from controllers.purposes_controller import purposes_blueprint
+# from controllers.purposes_controller import purposes_blueprint
+# from controllers.purposes_controller import purposes_blueprint
+
+app = Flask(__name__)
+
+app.register_blueprint(purposes_blueprint)
+
+@app.route("/")
+def main():
+    return render_template('index.html')
+
+if __name__ = '__main__':
+    app.run()
+    
