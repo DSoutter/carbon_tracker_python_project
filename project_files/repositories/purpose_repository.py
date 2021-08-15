2fbdcb40f71847fd4851c426bc64cf9ebbb57fa5
DSoutter <duncan.soutter@gmail.com>
2021-08-15 13:35:54 +0100

    select all purposes working

--- project_files/repositories/purpose_repository.py
+++ project_files/repositories/purpose_repository.py
@@ -14,7 +14,15 @@ def save(purpose):
 
 # select all
 
+def select_all():
+    purposes = []
+    sql = "SELECT * FROM purposes"
+    results = run_sql(sql)
+    for row in results:
 
+        purpose = Purpose(row['travel_purpose'], row['id'])
+        purposes.append(purpose)
+    return purposes
 
 # select
 
