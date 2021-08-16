f4034a0b385e259e9f54c1f95e65dbcecc59d9b7
DSoutter <duncan.soutter@gmail.com>
2021-08-16 11:22:34 +0100

    trips page working

--- project_files/controllers/trips_controller.py
+++ project_files/controllers/trips_controller.py
@@ -9,3 +9,28 @@ import repositories.transport_repository as transport_repo
 import repositories.purpose_repository as purpose_repo
 
 trip_blueprint = Blueprint("trips", __name__)
+
+# Index (all the trips on one screen)
+
+@trip_blueprint.route("/trips")
+def trips():
+    trips = trip_repo.select_all()
+    return render_template("trips/index.html", trips = trips)
+
+# New (add a new trip screen)
+
+
+
+# Create (posting the new trip to the list)
+
+
+
+# Edit Later if needed
+
+
+
+# Update Later if needed
+
+
+
+# Delete Later if needed
